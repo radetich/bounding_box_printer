e5531c9f726d7bd323b8859bfd5071680236c718
CJ Radetich <colejradetich@gmail.com>
2021-11-19 22:43:25 -0700

    Create yolo_2_cv.py

---
+++ yolo_2_cv.py
@@ -0,0 +1,85 @@
+from csv import reader
+import ast
+import cv2
+
+#self note: i need to not code at 2 am
+#MUST BE IN FOLDER WITH IMAGE FRAMES AND CSV OF YOLOv5 DETECTIONS
+
+#RAW DATA
+file_index = []
+filename = []
+min_pts = []
+max_pts = []
+conf = []
+name = []
+
+#CSV read driver, this parses the csv and gives us some lists (could have done a dictionary but I like my sanity)
+with open('detections.csv', 'r') as detections:
+    detection_list = reader(detections)
+    for index in detection_list:
+        file_index.append(int(index[1]))
+        filename.append(index[0])
+        min_pts.append((int(ast.literal_eval(index[2])["xmin"]), int(ast.literal_eval(index[2])["ymin"])))
+        max_pts.append((int(ast.literal_eval(index[2])["xmax"]), int(ast.literal_eval(index[2])["ymax"])))
+        conf.append(ast.literal_eval(index[2])["confidence"])
+        name.append(ast.literal_eval(index[2])["name"])
+
+for i in range(len(file_index)):
+#DEBUG
+#for i in range(2):
+    if(file_index[i] == 0):
+        tf = cv2.imread(filename[i]) 
+        start_point = min_pts[i]
+        end_point = max_pts[i]
+        tf = cv2.rectangle(tf, start_point, end_point, (255, 0, 0), 2) 
+        tf = cv2.putText(tf, "Name: "+name[i]+" Confidence: "+ str(100*conf[i])+"%", (int((min_pts[i][0] + max_pts[i][0])/2),int((min_pts[i][1] + max_pts[i][1])/2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imwrite(filename[i][90:], tf)
+
+    else:
+        tf = cv2.imread(filename[i][90:])
+        start_point = min_pts[i]
+        end_point = max_pts[i]
+        tf = cv2.rectangle(tf, start_point, end_point, (255, 0, 0), 2) 
+        tf = cv2.putText(tf, "Name: "+name[i]+" Confidence: "+ str(100*conf[i])+"%", (int((min_pts[i][0] + max_pts[i][0])/2),int((min_pts[i][1] + max_pts[i][1])/2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imwrite(filename[i][90:], tf)
+
+
+
+
+#HERE IS HOW WE MAKE A BOUNDING BOX
+
+#image = cv2.imread(filename[1]) 
+
+#start_point = min_pts[1]
+#end_point = max_pts[1]
+  
+# Blue color in BGR 
+#color = (255, 0, 0) 
+  
+# Line thickness of 2 px 
+#thickness = 2
+  
+# Using cv2.rectangle() method 
+# Draw a rectangle with blue line borders of thickness of 2 px 
+#image = cv2.rectangle(image, start_point, end_point, color, thickness) 
+#cv2.imwrite('test2.jpg', image) 
+
+#AND THE TEXT
+
+# font
+#font = cv2.FONT_HERSHEY_SIMPLEX
+  
+# org
+#org = (50, 50)
+  
+# fontScale
+#fontScale = 1
+   
+# Blue color in BGR
+#color = (255, 0, 0)
+  
+# Line thickness of 2 px
+#thickness = 2
+   
+# Using cv2.putText() method
+#image = cv2.putText(image, 'OpenCV', org, font, fontScale, color, thickness, cv2.LINE_AA)
